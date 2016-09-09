0b8bce8bdc2ca8b5cf17be876eee68ef6344f6dd
杨康 <147180@sh.lianjia.com>
2016-09-09 10:56:10 +0800

    修改日志文件的配置

--- src/main/java/com/eric/general/configuration/resource/AppPropertyConfigurer.java
+++ src/main/java/com/eric/general/configuration/resource/AppPropertyConfigurer.java
@@ -2,10 +2,8 @@ package com.eric.general.configuration.resource;
 import com.eric.general.common.util.IpUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -20,15 +18,6 @@ public class AppPropertyConfigurer extends PropertyPlaceholderConfigurer {
     protected Properties mergeProperties() throws IOException {
     	Properties superProps = super.mergeProperties();
         superProps.put("env", ConfigurationMap.getEvnDir());
-
-        File file = new File(ConfigurationMap.getClassRootPath() + superProps.getProperty("env") + "/log4j2.xml");
-
-        LoggerContext context =(LoggerContext) LogManager.getContext();
-        context.setConfigLocation(file.toURI());
-
-        //重新初始化Log4j2的配置上下文
-        context.reconfigure();
-
         LOGGER.info(">>>>> 运行环境：" + superProps.getProperty("env"));
         LOGGER.info(">>>>> 服务器IP：" + IpUtils.getLocalIP());
         return superProps;
